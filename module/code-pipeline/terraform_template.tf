7b686c71ace1b1e7c6fbbd9d1bbed464dfe62edb
Nextgent <45428335+Nextgent@users.noreply.github.com>
2019-06-07 15:24:31 +0530

    Modified by Workflow Agent

--- module/code-pipeline/terraform_template.tf
+++ module/code-pipeline/terraform_template.tf
@@ -161,7 +161,7 @@
         "aws_s3_bucket": {
             "source": {
                 "acl": "private",
-                "bucket": "aws-migration-app",
+                "bucket": "aws-app-migration-aws",
                 "force_destroy": true
             }
         }
@@ -169,7 +169,7 @@
     "terraform": {
         "backend": {
             "s3": {
-                "bucket": "aws-migration-app",
+                "bucket": "aws-app-migration-aws",
                 "key": "pipeline/terraform.tfstate",
                 "region": "us-east-1"
             }
