03eae7cdde3754ee1204003b0d7683579386a48b
Nextgent <45428335+Nextgent@users.noreply.github.com>
2019-06-10 12:44:31 +0530

    Modified by Workflow Agent

--- module/networking/terraform_template.tf
+++ module/networking/terraform_template.tf
@@ -184,7 +184,7 @@
   "terraform": {
     "backend": {
       "s3": {
-        "bucket": "aws-app-migration-aws",
+        "bucket": "aws-migration-app",
         "key": "networking/terraform.tfstate",
         "region": "us-east-1"
       }
